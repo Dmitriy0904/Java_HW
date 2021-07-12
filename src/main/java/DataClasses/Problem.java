7cd4abe436484ea12dde5eaf5b22f1f6ecf17bdb
Dmitriy0904 <62384390+Dmitriy0904@users.noreply.github.com>
2021-07-12 15:44:23 +0300

    Added data classes

---
+++ src/main/java/DataClasses/Problem.java
@@ -0,0 +1,31 @@
+package DataClasses;
+
+public class Problem {
+    private int id;
+    private int id_from;
+    private int id_to;
+
+    public int getId() {
+        return id;
+    }
+
+    public int getId_from() {
+        return id_from;
+    }
+
+    public int getId_to() {
+        return id_to;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setId_from(int id_from) {
+        this.id_from = id_from;
+    }
+
+    public void setId_to(int id_to) {
+        this.id_to = id_to;
+    }
+}
---
+++ src/main/java/DataClasses/Solution.java
@@ -0,0 +1,22 @@
+package DataClasses;
+
+public class Solution {
+    private int problem_id;
+    private int cost;
+
+    public int getProblem_id() {
+        return problem_id;
+    }
+
+    public int getCost() {
+        return cost;
+    }
+
+    public void setProblem_id(int problem_id) {
+        this.problem_id = problem_id;
+    }
+
+    public void setCost(int cost) {
+        this.cost = cost;
+    }
+}
