c7a4aa11bfc4a98a163bd1e9e2dfd83db3af0cbf
Dmitriy0904 <62384390+Dmitriy0904@users.noreply.github.com>
2021-07-12 22:56:10 +0300

    Added classes to build an adjacency matrix and find the shortest path

---
+++ src/main/java/ShortestPath/AdjacencyMatrix.java
@@ -0,0 +1,25 @@
+package ShortestPath;
+
+import DataClasses.Location;
+import DataClasses.Route;
+import java.util.List;
+
+public class AdjacencyMatrix {
+
+    public static Integer[][] buildMatrix(List<Route> routes, List<Location> cities){
+        Integer [][] adjacencyMatrix = new Integer[cities.size()][cities.size()];
+
+        for (Route item : routes){
+            adjacencyMatrix[item.getId_from() - 1][item.getId_to() - 1] = item.getCost();
+        }
+
+        for(int i = 0; i < cities.size(); i++){
+            for(int j = 0; j < cities.size(); j++){
+                if(adjacencyMatrix[i][j] == 0){
+                    adjacencyMatrix[i][j] = Integer.MAX_VALUE;
+                }
+            }
+        }
+        return adjacencyMatrix;
+    }
+}
---
+++ src/main/java/ShortestPath/ShortestPath.java
@@ -0,0 +1,54 @@
+package ShortestPath;
+
+import DataClasses.Location;
+import DataClasses.Problem;
+import DataClasses.Route;
+import DataClasses.Solution;
+import java.util.List;
+import static java.lang.Math.min;
+import static java.util.Arrays.fill;
+
+public class ShortestPath {
+
+    public static Solution findShortestPath(List<Route> routes, List<Location> locations, Problem problem){
+        Integer[][]adjacencyMatrix = AdjacencyMatrix.buildMatrix(routes, locations);
+
+        Solution solution = new Solution();
+        solution.setProblem_id(problem.getId());
+
+        int start = problem.getId_from() - 1;
+        int end = problem.getId_to() - 1;
+
+        int []shortestPaths = dijkstraAlgorithm(adjacencyMatrix, start);
+        int resultPath = shortestPaths[end];
+
+        solution.setCost(resultPath > 200_000 ? null : resultPath);
+        return solution;
+    }
+
+
+    private static int[] dijkstraAlgorithm(Integer[][] adjacencyMatrix, int start) {
+        int INF = Integer.MAX_VALUE / 2;
+        int vNum = adjacencyMatrix.length;
+        boolean[] used = new boolean [vNum];
+        int[] dist = new int [vNum];
+
+        fill(dist, INF);
+        dist[start] = 0;
+
+        for (;;) {
+            int v = -1;
+            for (int nv = 0; nv < vNum; nv++)
+                if (!used[nv] && dist[nv] < INF && (v == -1 || dist[v] > dist[nv]))
+                    v = nv;
+            if (v == -1) break;
+            used[v] = true;
+            for (int nv = 0; nv < vNum; nv++)
+                if (!used[nv] && adjacencyMatrix[v][nv] < INF)
+                    dist[nv] = min(dist[nv], dist[v] + adjacencyMatrix[v][nv]);
+        }
+
+        return dist;
+    }
+
+}
