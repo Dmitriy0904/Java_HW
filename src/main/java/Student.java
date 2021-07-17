cea1c2c2dd2bf8f2e088e0500505267e471a469e
Dmitriy0904 <62384390+Dmitriy0904@users.noreply.github.com>
2021-07-17 16:21:46 +0300

    Added data class

---
+++ src/main/java/Student.java
@@ -0,0 +1,53 @@
+public class Student {
+    @PropertyKey("firstname")
+    private String firstname;
+
+    @PropertyKey("lastname")
+    private String lastname;
+
+    @PropertyKey("faculty")
+    private String faculty;
+
+    @PropertyKey("course")
+    private int course;
+
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public String getFaculty() {
+        return faculty;
+    }
+
+    public int getCourse() {
+        return course;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public void setFaculty(String faculty) {
+        this.faculty = faculty;
+    }
+
+    public void setCourse(int course) {
+        this.course = course;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" + firstname + " " + lastname +
+                ", course:" + course +
+                ", faculty:" + faculty + '}';
+    }
+}
