9925b772c41b08b3fe542a3634d1c9a8c18cb166
Dmitriy0904 <62384390+Dmitriy0904@users.noreply.github.com>
2021-07-19 16:48:48 +0300

    Added entity

---
+++ src/main/java/model/Student.java
@@ -0,0 +1,80 @@
+package model;
+
+import annotation.Mapped;
+
+public class Student {
+    @Mapped("firstname")
+    private String firstname;
+
+    @Mapped("lastname")
+    private String lastname;
+
+    @Mapped("faculty")
+    private String faculty;
+
+    @Mapped("course")
+    private Integer course;
+
+    @Mapped("group")
+    private String group;
+
+    @Mapped("scholarship")
+    private Boolean scholarship;
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public String getFaculty() {
+        return faculty;
+    }
+
+    public Integer getCourse() {
+        return course;
+    }
+
+    public String getGroup() {
+        return group;
+    }
+
+    public Boolean getScholarship() {
+        return scholarship;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public void setFaculty(String faculty) {
+        this.faculty = faculty;
+    }
+
+    public void setCourse(Integer course) {
+        this.course = course;
+    }
+
+    public void setGroup(String group) {
+        this.group = group;
+    }
+
+    public void setScholarship(Boolean scholarship) {
+        this.scholarship = scholarship;
+    }
+
+    @Override
+    public String toString() {
+        return  firstname + " " + lastname +
+                " faculty:" + faculty +
+                " course: " + course +
+                " group: " + group +
+                " scholarship: " + scholarship;
+    }
+}
