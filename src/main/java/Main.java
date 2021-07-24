76de5df18418f852d6ba0cddc79e6f19a0c19f65
Dmitriy0904 <62384390+Dmitriy0904@users.noreply.github.com>
2021-07-24 11:58:37 +0300

    Changed strategy of getting path to .csv file

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
     public static void main(String[] args) {
-        Application.run();
+        if(args.length == 1){
+            Application.run(args[0]);
+        } else {
+            try(BufferedReader bf = new BufferedReader(new InputStreamReader(System.in))){
+                System.out.println("Enter path to the csv file:");
+                String csvPath = bf.readLine();
+                Application.run(csvPath);
+            } catch (IOException exception){
+                throw new RuntimeException(exception);
+            }
+        }
     }
 }
--- src/main/java/mapper/CsvReader.java
+++ src/main/java/mapper/CsvReader.java
@@ -10,28 +10,27 @@ import java.io.IOException;
 import java.util.List;
 
 public class CsvReader {
-    private static final String PATH = "csv/students.csv";
     private static final Logger log = LoggerFactory.getLogger(CsvReader.class);
 
-    public List<String[]> readFile(){
+    public List<String[]> readFile(String path){
         List<String[]> info;
 
-        log.info("Try to read from csv file: {}", PATH);
+        log.info("Try to read from csv file: {}", path);
 
-        try(CSVReader reader = new CSVReader(new FileReader(PATH))) {
+        try(CSVReader reader = new CSVReader(new FileReader(path))) {
 
             info = reader.readAll();
 
         } catch (FileNotFoundException exception) {
-            log.error("FileNotFoundException in read csv file: {}", PATH);
+            log.error("FileNotFoundException in read csv file: {}", path);
             throw new RuntimeException(exception);
 
         } catch (IOException exception) {
-            log.error("IOException in read csv file: {}", PATH);
+            log.error("IOException in read csv file: {}", path);
             throw new RuntimeException(exception);
 
         } catch (CsvException exception) {
-            log.error("CsvException in read csv file: {}", PATH);
+            log.error("CsvException in read csv file: {}", path);
             throw new RuntimeException(exception);
         }
 
