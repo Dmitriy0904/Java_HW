4d2ab1e5a597ae8ed894c09fa6753c2de30b618b
Dmitriy0904 <62384390+Dmitriy0904@users.noreply.github.com>
2021-07-31 03:58:10 +0300

    Added classes to run the application

---
+++ src/main/java/Application.java
@@ -0,0 +1,8 @@
+import controller.Controller;
+
+public class Application {
+    public static void run(){
+        Controller controller = new Controller();
+        controller.userInterface();
+    }
+}
